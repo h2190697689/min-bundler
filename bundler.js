3ba4268a9d20ff1d8b312c9f4ff5f1bdf3d25c0c
h2190697689 <2190697689@qq.com>
2019-10-18 19:40:04 +0800

    second commit

--- bundler.js
+++ bundler.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
+const path = require("path");
 const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const babel = require("@babel/core");
 
 const moduleAnalyser = (filename) => {
     const content = fs.readFileSync(filename,"utf-8");
     console.log(content);
-    console.log(parser.parse(content));
+    const ast = parser.parse(content, {
+        sourceType: "module"
+    })
+    console.log(ast);
 }
 
 moduleAnalyser("./src/index.js")
